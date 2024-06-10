5d5269e8b3ee30c012fc9adc0e84dc8948c58393
andxbes <andxbes@gmail.com>
2024-06-10 23:35:46 +0300

    219

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,19 @@
 import classes from './User.module.css';
+import { Component } from 'react';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  constructor() {
+    super();
+    console.info('user constructor');
+  }
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
