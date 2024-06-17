575d2922afa9da261e20004b16431269294ec0dc
andxbes <andxbes@gmail.com>
2024-06-17 20:07:38 +0300

    223

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -2,26 +2,29 @@ import { Fragment, Component } from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
+import UsersContext from '../store/users-context';
 
-const DUMMY_USERS = [
-    { id: 'u1', name: 'Max' },
-    { id: 'u2', name: 'Manuel' },
-    { id: 'u3', name: 'Julie' },
-];
 class UserFinder extends Component {
+    static contextType = UsersContext;
     constructor() {
         super();
         this.state = {
-            filteredUsers: DUMMY_USERS,
+            filteredUsers: this.context.users,
             searchTerm: ''
         };
+    }
+
+    componentDidMount() {
+        this.setState({
+            filteredUsers: this.context.users,
+        });
     }
 
     //analog useEffect
     componentDidUpdate(prevProps, prevState) {
         if (prevState.searchTerm !== this.state.searchTerm) {
             this.setState({
-                filteredUsers: DUMMY_USERS.filter((user) =>
+                filteredUsers: this.context.users.filter((user) =>
                     user.name.includes(this.state.searchTerm)
                 ),
             });
